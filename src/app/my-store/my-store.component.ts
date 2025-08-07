57a207ccfd4432701b92b0242963241caafbe10e
Ariel <avranico@gmail.com>
2025-08-07 19:12:07 +0300

    create products service

--- src/app/my-store/my-store.component.ts
+++ src/app/my-store/my-store.component.ts
@@ -3,7 +3,8 @@ import { HeaderComponent } from '../header/header.component';
 import { FiltersPanelComponent } from '../filters-panel/filters-panel.component';
 import { ProductsListComponent } from '../products-list/products-list.component';
 import { DetailsPanelComponent } from '../details-panel/details-panel.component';
-import { RestApiService } from '../services/rest-api.service';
+
+import { ProductsService } from '../services/products.service';
 @Component({
   selector: 'app-my-store',
   imports: [HeaderComponent, FiltersPanelComponent, ProductsListComponent, DetailsPanelComponent],
@@ -11,11 +12,9 @@ import { RestApiService } from '../services/rest-api.service';
   styleUrl: './my-store.component.scss'
 })
 export class MyStoreComponent {
-  restApi = inject(RestApiService);
+  productService = inject(ProductsService);
 
   ngOnInit() {
-    this.restApi.getProducts().subscribe(res=>{
-      console.log(res);
-    })
+    this.productService.loadInit();
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product.model';
+import { RestApiService } from '../services/rest-api.service';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  restApi = inject(RestApiService);
 
- 
+  loadInit() {
+    this.restApi.getProducts().subscribe(res => {
+      console.log(res);
+    })
+  }
+
 }
