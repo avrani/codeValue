e448dfb66f3f79438760f77b891ebceff2feb796
Ariel <avranico@gmail.com>
2025-08-07 17:29:30 +0300

    adding header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MyStoreComponent } from './my-store/my-store.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'my-store', pathMatch: 'full' }, //default route
+    { path: 'my-store', component: MyStoreComponent },
+    { path: '**', component: MyStoreComponent }
+];
---
+++ src/app/my-store/my-store.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+@Component({
+  selector: 'app-my-store',
+  imports: [HeaderComponent],
+  templateUrl: './my-store.component.html',
+  styleUrl: './my-store.component.scss'
+})
+export class MyStoreComponent {
+
+}
