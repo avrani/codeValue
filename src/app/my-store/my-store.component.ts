7355fa278e97d25d0825691371409e5c57da39bc
Ariel <avranico@gmail.com>
2025-08-07 17:37:03 +0300

    adding main comonenets

--- src/app/my-store/my-store.component.ts
+++ src/app/my-store/my-store.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FiltersPanelComponent } from '../filters-panel/filters-panel.component';
+import { ProductComponent } from '../product/product.component';
+import { DetailsPanelComponent } from '../details-panel/details-panel.component';
 @Component({
   selector: 'app-my-store',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,FiltersPanelComponent,ProductComponent,DetailsPanelComponent],
   templateUrl: './my-store.component.html',
   styleUrl: './my-store.component.scss'
 })
